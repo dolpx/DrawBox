546e996a9db3c41f51032acefebe9ee10859c708
dolpx <119837685+dolpx@users.noreply.github.com>
2023-01-28 12:27:47 +0800

    Draw_zfx.py
    Draw zfx

---
+++ PythonBox_Draw/Draw_zfx.py
@@ -0,0 +1,16 @@
+"""
+=====================
+[[[ DRAW_BOX v1.0 ]]]
+=====================
+"""
+
+import turtle 
+def Draw_zfx():
+	a = turtle.Pen()
+	for i in range(4):
+		a.fd(100)
+		a.rt(90)
+	a.down()
+
+
+Draw_zfx()
